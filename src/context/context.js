31c98e310e5d5f81f8c6306c5b33d3afb04a22cf
Shaw Lu <shawlu95@gmail.com>
2022-06-12 21:36:32 -0700

    Setup global context

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,15 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+// provider, consumer - GithubContext.Provider
+// wrap whole application (children) with globally accessible value
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+  );
+};
+
+export { GithubProvider, GithubContext };
