ada04612b011eedbcaa12558c38830e77d0787c3
Shaw Lu <shawlu95@gmail.com>
2022-06-12 22:26:01 -0700

    Update static data
    GitHub imposes rate limit of 60 request / hr. Use static data to develop.

--- src/context/context.js
+++ src/context/context.js
@@ -11,8 +11,13 @@ const GithubContext = React.createContext();
 // provider, consumer - GithubContext.Provider
 // wrap whole application (children) with globally accessible value
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepose] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
