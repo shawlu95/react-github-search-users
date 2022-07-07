a9fd7f015612adc0561e3f35db04f4cdf22d666c
Shaw Lu <shawlu95@gmail.com>
2022-07-06 22:45:26 -0700

    Create column and bar chart (stars & forks)

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import ReactFC from 'react-fusioncharts';
 
-const Bar3D = () => {
-  return <div>chart</div>;
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.ocean';
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  // STEP 3 - Creating the JSON object to store the chart configurations
+  const chartConfigs = {
+    type: 'bar2d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      chart: {
+        caption: 'Most Forked',
+        xAxisName: 'Repos',
+        yAxisName: 'Forks',
+        xAxisNameFontSize: '16px',
+        yAxisNameFontSize: '16px',
+        theme: 'ocean',
+      },
+      // Chart Data
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Bar3D;
+export default ChartComponent;
