10cb3ce9e22f580792a060c6f2329ffda0a6c5e3
Shaw Lu <shawlu95@gmail.com>
2022-07-06 23:18:58 -0700

    Fetch rate limit

--- src/context/context.js
+++ src/context/context.js
@@ -14,8 +14,33 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepose] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  useEffect(() => {
+    const checkRequests = async () => {
+      const {
+        data: {
+          rate: { remaining },
+        },
+      } = await axios.get(`${rootUrl}/rate_limit`);
+      setRequests(remaining);
+      if (remaining === 0) {
+        toggleError(true, 'Exceeded hourly rate!');
+      }
+    };
+    checkRequests();
+  }, []);
+
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   );
